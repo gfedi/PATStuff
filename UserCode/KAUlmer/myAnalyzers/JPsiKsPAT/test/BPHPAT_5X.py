6c72aeb286011358c0f5cc29a78fba3b038d3c3d
Giacomo Fedi <giacomo.fedi@cern.ch>
2013-07-25 18:39:42 +0200

    Updated PAT code for 5_x

---
+++ UserCode/KAUlmer/myAnalyzers/JPsiKsPAT/test/BPHPAT_5X.py
@@ -0,0 +1,128 @@
+runMC = True
+
+## import skeleton process
+from PhysicsTools.PatAlgos.patTemplate_cfg import *
+from PhysicsTools.PatAlgos.tools.coreTools import *
+
+process.maxEvents = cms.untracked.PSet(  input = cms.untracked.int32(-1) )
+
+if (runMC):
+   process.source = cms.Source("PoolSource",
+			    duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+			    skipEvents = cms.untracked.uint32(0),
+			    fileNames = cms.untracked.vstring(
+   # one file from: /RelValQCD_Pt_80_120/CMSSW_4_1_5-START311_V2-v1/GEN-SIM-RECO
+   #	'file:/nfs/data36/cms/ulmerk/CMSSW_4_1_5/src/EC43622A-746F-E011-8800-0018F3D09664.root'
+   # one file from /RelValJpsiMM/CMSSW_4_2_2-START42_V11-v1/GEN-SIM-RECO
+        'file:/nfs/data36/cms/ulmerk/test_temp/CMSSW_4_2_2/src/109D8F99-B16D-E011-A69A-0018F3D0968C.root'
+	)
+   )
+   process.GlobalTag.globaltag = cms.string('START311_V2::All')
+   #process.GlobalTag.globaltag = cms.string('START42_V11::All')
+
+else:
+   process.source = cms.Source("PoolSource",
+			    duplicateCheckMode = cms.untracked.string('noDuplicateCheck'),
+			    skipEvents = cms.untracked.uint32(0),
+			    fileNames = cms.untracked.vstring(
+	'/store/data/Run2011A/SingleMu/RECO/PromptReco-v1/000/161/008/0E573388-C555-E011-B956-001D09F24FBA.root'
+	)
+   )
+   process.GlobalTag.globaltag = cms.string('GR_P_V17::All')
+
+process.load("PhysicsTools.PatAlgos.cleaningLayer1.genericTrackCleaner_cfi")
+process.cleanPatTracks.checkOverlaps.muons.requireNoOverlaps = cms.bool(False)
+process.cleanPatTracks.checkOverlaps.electrons.requireNoOverlaps = cms.bool(False)
+
+process.load("PhysicsTools.PatAlgos.mcMatchLayer0.muonMatch_cfi")
+process.muonMatch.maxDeltaR = cms.double(0.02)
+process.muonMatch.resolveByMatchQuality = cms.bool(True)
+
+## add track candidates
+from PhysicsTools.PatAlgos.tools.trackTools import *
+
+if (runMC):
+   makeTrackCandidates(process,
+       label        = 'TrackCands',                  
+       tracks       = cms.InputTag('generalTracks'), 
+       particleType = 'pi+',                         
+       preselection = 'pt > 0.1',                     
+       selection    = 'pt > 0.1',                     
+       isolation    = {},                            
+       isoDeposits  = [],                            
+       mcAs         = 'muon'           
+   )                      
+   process.patTrackCandsMCMatch.mcPdgId = cms.vint32(211)
+   process.patTrackCandsMCMatch.mcStatus = cms.vint32(1)
+   process.patTrackCandsMCMatch.resolveByMatchQuality = cms.bool(True)
+   process.patTrackCandsMCMatch.maxDeltaR = cms.double(0.02)   
+
+
+else:
+    makeTrackCandidates(process,
+       label        = 'TrackCands',                  
+       tracks       = cms.InputTag('generalTracks'), 
+       particleType = 'pi+',                         
+       preselection = 'pt > 0.1',                     
+       selection    = 'pt > 0.1',                     
+       isolation    = {},                            
+       isoDeposits  = [],                            
+       mcAs         = None          
+   )    
+             
+
+
+if not(runMC):
+   removeMCMatching(process, ['All'])
+
+process.patDefaultSequence.remove(process.patJetCorrFactors)
+process.patDefaultSequence.remove(process.patJetCharge)
+process.patDefaultSequence.remove(process.patJetPartonMatch)
+process.patDefaultSequence.remove(process.patJetGenJetMatch)
+process.patDefaultSequence.remove(process.patJetPartons)
+process.patDefaultSequence.remove(process.patJetPartonAssociation)
+process.patDefaultSequence.remove(process.patJetFlavourAssociation)
+process.patDefaultSequence.remove(process.patJets)
+process.patDefaultSequence.remove(process.patMETs)
+process.patDefaultSequence.remove(process.selectedPatJets)
+process.patDefaultSequence.remove(process.cleanPatJets)
+process.patDefaultSequence.remove(process.countPatJets)
+
+
+# do trigger matching between PAT muons and HLT muons
+process.cleanMuonTriggerMatchHLT = cms.EDProducer(
+  "PATTriggerMatcherDRLessByR"		  # match by DeltaR only, best match by DeltaR
+, src	  = cms.InputTag( "cleanPatMuons" )
+, matched = cms.InputTag( "patTrigger" )	  # default producer label as defined in PhysicsTools/PatAlgos/python/triggerLayer1/triggerProducer_cfi.py
+, matchedCuts = cms.string( 'type( "TriggerMuon" )' )
+, maxDeltaR = cms.double( 0.10 )
+, resolveAmbiguities	= cms.bool( False )	  # only one match per trigger object
+, resolveByMatchQuality = cms.bool( False )	  # take best match found per reco object: by DeltaR here (s. above)
+)
+
+# Embedding in muons
+process.cleanPatMuonsTriggerMatch = cms.EDProducer(
+  "PATTriggerMatchMuonEmbedder"
+, src     = cms.InputTag( "cleanPatMuons" )
+, matches = cms.VInputTag(
+   'cleanMuonTriggerMatchHLT'
+  )
+)
+process.trigMatch = cms.Sequence(process.cleanMuonTriggerMatchHLT *process.cleanPatMuonsTriggerMatch )
+
+
+## let it run
+process.pat = cms.Path(
+    process.patDefaultSequence
+)
+
+# switch on PAT trigger info
+from PhysicsTools.PatAlgos.tools.trigTools import *
+switchOnTrigger( process )
+
+# Schedule
+process.trigMatchPath = cms.Path( process.trigMatch )
+if (runMC):
+   process.schedule = cms.Schedule(process.pat, process.trigMatchPath )
+else:
+   process.schedule = cms.Schedule(process.pat, process.trigMatchPath )
